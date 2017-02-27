159ff1a23729bebeff36c6c5d834c19f04f28f01
Vadiwoo <karuppiv@oregonstate.edu>
2017-02-26 23:11:33 -0800

    Create Pinterest.js

---
+++ js/Pinterest.js
@@ -0,0 +1,24 @@
+  
+  /*
+Name             : Vadiwoo Karuppiah
+
+Assignment Title : API-How-to Guide
+
+Date              : February,13, 2017 
+*/
+
+  var body = document.getElementsByTagName("body")[0];
+
+  var host = document.getElementsByTagName("results");
+
+
+  var chapter1 = document.createElement("p");
+  var intro1= "Pinterest is the world’s catalog of ideas.We can find and save recipes, parenting hacks,style inspiration and other ideas to try."
+  var intro2 = "This tutorial will show you how to navigate and make use of Pinterest's API using JavaScript. It is intended for beginners with some JavaScript background."
+  var intro3 = "In order to use our API, you’ll need to create an app. Once you’ve created an app, you’ll be able to test it and add collaborators. When you think your app is ready, you can submit it for review.";
+  var chapter1Text = document.createTextNode(intro1 + intro2 + intro3);
+ chapter1.appendChild(chapter1Text);
+  introduction.appendChild(chapter1);
+
+
+   
